cd042f30a611ea00629f1550e427572159058ba0
Aakash Sharma <aakashagileinfoways@gmail.com>
2026-05-11 15:55:29 +0530

    changes in data-for thee development processs

--- src/main/java/com/fooddelivery/config/WebConfig.java
+++ src/main/java/com/fooddelivery/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @Override
+            @Override 
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
